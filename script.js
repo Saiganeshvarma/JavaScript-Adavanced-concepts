9cf8a0d40c36c0c3c6dc88153b9f337300085561
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-02-07 16:21:39 +0530

    array methods

--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-//promises in the java script
+// //promises in the java script
 
-// create the promise
+// // create the promise
 
-var mypromise = new Promise((resolve,reject)=>{
-    var isright = true
-    if(isright == true){
-        resolve("yes she is right")
-    }else{
-        reject("she is wrong")
-    }
-})
-// handling the promise
-mypromise
-.then((result)=>{
-    console.log(result);
-})
-.catch((result)=>{
-    console.log(result);
-})
-.finally(()=>{
-    console.log("code executed");
-})
+// var mypromise = new Promise((resolve,reject)=>{
+//     var isright = true
+//     if(isright == true){
+//         resolve("yes she is right")
+//     }else{
+//         reject("she is wrong")
+//     }
+// })
+// // handling the promise
+// mypromise
+// .then((result)=>{
+//     console.log(result);
+// })
+// .catch((result)=>{
+//     console.log(result);
+// })
+// .finally(()=>{
+//     console.log("code executed");
+// })
 
- //try and catch in java script
+//  //try and catch in java script
 
 
-try{
-  var k = fdsfsdfgh
-   console.log(k);
- }catch(error){
-     console.log("k will show errror");
+// try{
+//   var k = fdsfsdfgh
+//    console.log(k);
+//  }catch(error){
+//      console.log("k will show errror");
 
-}
+// }
+
+// map
+
+// var array = [1,2,3,4,5]
+// var newArray = array.map((item)=>{
+//     return item>10
+
+// })
+// console.log(newArray);
+
+// filter
+// var array = [1,2,3,4,5]
+// var newArray = array.filter((item)=>{
+//     return item>4
+// })
+// console.log(newArray);
+
+// find
+// var array = [1,2,3,4,5]
+// var newArray = array.find((item)=>{
+//     return item>4
+// })
+// console.log(newArray);
+
+// reduce
+// var array = [1,2,3,4,5]
+// var newArray = array.reduce((a,b)=>{
+//     console.log(a,b);
+//     return a+b
+// })
+// console.log(newArray);
+
+
+// foreach
+// var array = [1,2,3,4,5]
+// array.forEach((item)=>{
+//     console.log(item);
+// })
+// for(var i = 0 ; i<array.length ; i++){
+//     console.log(array[i]);
+// }
